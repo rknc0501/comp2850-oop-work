59ead973a3b9049945293d23593971d5ec395edf
rknc0501 <rknc0501@leeds.ac.uk>
2025-10-06 14:30:29 +0000

    NIce one

--- tasks/task1_2/Greek.kt
+++ tasks/task1_2/Greek.kt
@@ -1,5 +1,4 @@
-fun main()
-{
-    val text = "Joe";
-    println(text);
+fun greetingFor(target: String): String {
+    val greeting = setOf("Hello", "Hi", "G'day").random()
+    return "$greeting $target!"
 }
--- tasks/task1_2/Hello.kt
+++ tasks/task1_2/Hello.kt
@@ -1,5 +1,8 @@
-fun main()
-{
-    var text = "Hello";
-    println(text);
+fun main(args: Array<String>) {
+    if (args.isNotEmpty()) {
+        println(greetingFor(args[0]))
+    }
+    else {
+        println(greetingFor("World"))
+    }
 }
