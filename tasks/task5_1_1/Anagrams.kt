beb360276a8b7607c7c4959a7d6beae82d14542f
rknc0501 <rknc0501@leeds.ac.uk>
2025-10-06 15:04:43 +0000

    .

--- tasks/task5_1_1/Anagrams.kt
+++ tasks/task5_1_1/Anagrams.kt
@@ -1,1 +1,31 @@
 // Task 5.1.1: anagram checking using a function
+fun anagrams(first: String, second: String): Boolean {
+    if (first.length != second.length) {
+        return false
+    }
+    val firstChars = first.lowercase().toList().sorted()
+    val secondChars = second.lowercase().toList().sorted()
+    return firstChars == secondChars
+}
+
+fun main(args: Array<String>)
+{
+    println("Enter a word")
+    var FirstLetter = readln()
+    println("Enter a second letter that have the same length with with word")
+    var SecondLetter = readln()
+    println(FirstLetter +" "+SecondLetter )
+    var isAnagrams:Boolean = anagrams(FirstLetter,SecondLetter)
+    if(isAnagrams == true)
+    {
+        println("They are anagram")
+    }
+    else if(isAnagrams == false)
+    {
+        println("They are not anagram")
+    }
+
+
+
+
+}
