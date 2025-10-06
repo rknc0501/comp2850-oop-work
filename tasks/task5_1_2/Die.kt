249314893e52a981980f664791f9121f9223f260
rknc0501 <rknc0501@leeds.ac.uk>
2025-10-06 15:04:55 +0000

    .

--- tasks/task5_1_2/Die.kt
+++ tasks/task5_1_2/Die.kt
@@ -1,1 +1,12 @@
 // Task 5.1.2: die rolling simulation
+fun rollDie(sides: Int) {
+    if (sides in setOf(4, 6, 8, 10, 12, 20)) {
+        println("Rolling a d$sides...")
+        val result = Random.nextInt(1, sides + 1)
+        println("You rolled $result")
+    }
+    else {
+        println("Error: cannot have a $sides-sided die")
+    }
+}
+
