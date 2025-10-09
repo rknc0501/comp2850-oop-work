ef3f09e0ab767e5d1039b42619a11e2b99675574
rknc0501 <rknc0501@leeds.ac.uk>
2025-10-09 14:50:18 +0000

    .

--- tasks/task7_2/Array.kt
+++ tasks/task7_2/Array.kt
@@ -1,9 +1,16 @@
 fun main() 
 {
     val numbers = arrayOf(9, 6, 3, 2)
-
+    val num = IntArray(10)
     val cls = numbers::class
+
+    val arr = Array(5) { 0 }          // Int array of size 5, all 0
+    val arr2 = arrayOf(1, 2, 3, 4, 5) // Direct initialization
+    val arr3 = IntArray(5)   
 
     println(cls.qualifiedName)
     println(cls.java)
+    println(num.size) //create an empty array with fixed size
+
+
 }
--- tasks/task7_3_1/List.kt
+++ tasks/task7_3_1/List.kt
@@ -1,1 +1,19 @@
 // Task 7.3.1: list element access
+fun main()
+{
+   val number = List<Float>(100) { 0.0f }
+   val greetings = List<String>(5) { "Hello" }
+   val numbers = mutableListOf(9, 3, 6, 2)  //mutable makes the list editable instead of fixed list
+   val names = listOf("Nine", "Three", "Six", "Two")
+   val age = mutableListOf<Int> () // empty mutabnle list 
+
+   println(numbers)
+   println(numbers[0])
+   println(numbers.get(0))
+   println(numbers.slice(1..2))
+   println("${numbers.first()} ${numbers.last()}")
+   numbers.add(3)
+   println(numbers.size)
+   println(value.size)
+   println(age.size)
+} 
