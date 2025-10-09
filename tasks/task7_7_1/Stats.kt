8fbdabe881bc54535299817798357253cae0ed15
rknc0501 <rknc0501@leeds.ac.uk>
2025-10-09 15:45:20 +0000

    .

--- tasks/task7_7_1/Stats.kt
+++ tasks/task7_7_1/Stats.kt
@@ -1,1 +1,36 @@
 // Task 7.7.1: stats for a numeric dataset
+fun readData() : List<Float>
+{
+    var numberSet = mutableListOf<Float>()
+    // Print a prompt for the user
+    var number: Float = 0.0f
+    do{
+        println("Enter a number, to stop enter 0")
+        number = readln().toFloat()
+        if(number == 0.0f)
+        {
+            println("END")
+        }
+        else
+        {
+            numberSet.add(number)
+        }
+
+    }while(number!=0.0f)
+
+    return numberSet
+    // Write a loop to read the numbers
+    // Inside this loop, call add() to add a number to list
+}
+
+fun sortMedian(List<Float> numberSet) : Float
+{
+   numberSet.sortMedian()
+
+}
+
+
+fun main(args:Array<String>)
+{
+     
+}
