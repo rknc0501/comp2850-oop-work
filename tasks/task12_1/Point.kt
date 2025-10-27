2a984ef4ebec1dad68932e2ed9206bce2503cb5a
rknc0501 <rknc0501@leeds.ac.uk>
2025-10-27 16:42:41 +0000

    .

--- tasks/task12_1/Point.kt
+++ tasks/task12_1/Point.kt
@@ -3,17 +3,21 @@ import kotlin.math.hypot
 class Point(var x: Double, var y: Double) 
 {
     fun distance() = hypot(x, y)
+
+    fun distanceTo(other: Point): Double {
+        return hypot(x - other.x, y - other.y)
+    }
 }
 
-fun 
 
 fun main(args:Array<String>)
 {
-    val p = Point(10.0,5.0)
-    
+   val p = Point(10.0,5.0)
 
-println(p.x)   // prints 0.0
-println(p.y)   // prints 0.0
-println(p.distance())
+   println(p.x)   // prints 0.0
+   println(p.y)   // prints 0.0
+   println(p.distance())
+   val x = Point(3.0,3.0)
+   println(p.distanceTo(x))
 
 }
