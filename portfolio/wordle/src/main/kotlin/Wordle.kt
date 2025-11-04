4e1c26bde83ea94c7bb1ee428d81570da96c9c5a
rknc0501 <rknc0501@leeds.ac.uk>
2025-11-04 11:30:18 +0000

    wordle

--- portfolio/wordle/src/main/kotlin/Wordle.kt
+++ portfolio/wordle/src/main/kotlin/Wordle.kt
@@ -1,1 +1,42 @@
-// Implement the six required functions here
+import java.io.File
+import java.io.FileNotFoundException
+import kotlin.random.Random
+
+fun isValid(word: String): Boolean
+{
+    if(word.length == 5 && word.all{it.isLetter()})
+    {
+        return true
+    }
+    else
+    {
+        return false
+    }
+}
+
+fun readWordList(filename: String): MutableList<String>
+{
+    try
+    {
+        val word_set : MutableList<String> = File(filename).readLines().toMutableList()
+        return word_set
+    }
+    catch (e: FileNotFoundException)
+    {
+        println("Error! ${filename} not found.")
+        return mutableListOf()
+    }
+}
+
+
+fun pickRandomWord(words: MutableList<String>): String
+{
+     var Random_num = Random.nextInt(0,words.size)
+     val selected_word = words[Random_num]
+     return selected_word
+}
+
+fun evaluateGuess(guess: String, target: String): List<Int>
+{
+    
+}
