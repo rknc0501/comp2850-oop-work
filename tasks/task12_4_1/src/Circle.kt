e3a2ae366c6eaddb12e7cdbb7f3684bd905cd4e2
rknc0501 <rknc0501@leeds.ac.uk>
2025-11-04 10:07:52 +0000

    .

--- tasks/task12_4_1/src/Circle.kt
+++ tasks/task12_4_1/src/Circle.kt
@@ -1,1 +1,19 @@
 // Task 12.4.1: Circle class
+import kotlin.math.PI
+
+class Circle(val centre: Point, val radius: Double)
+{
+     init{
+         require(radius > 0)
+     }
+
+     fun area(): Double 
+     {
+        return radius*radius*PI
+     }
+
+     fun perimeter(): Double 
+     {
+        return 2*radius*PI
+     }
+}
